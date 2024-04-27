eb6c687ebaef990a2e8faedc3f009c95e0baff3a
GitKid117 <kgosiphenyo@gmail.com>
2024-04-27 22:04:31 +0200

    Random number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+
+        Console.Write("What is your guess? ");
+        string response = Console.ReadLine();
+        int guess = int.Parse(response);
+
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            Console.Write("What is your guess? ");
+            response = Console.ReadLine();
+            guess = int.Parse(response);
+        }
+        Console.WriteLine("You guessed it!");
+
     }
 }
